cbcb7d8aa89bab1d5990359164610af362d0364c
DaiTensa <dai.tensaout@gmail.com>
2025-06-18 14:41:08 +0200

    Ajout deux items url & text

--- scraper/pbfcrwal/pbfcrwal/items.py
+++ scraper/pbfcrwal/pbfcrwal/items.py
@@ -8,5 +8,6 @@ import scrapy
 
 class PbfcrwalItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    url = scrapy.Field()
+    text = scrapy.Field()
     pass
